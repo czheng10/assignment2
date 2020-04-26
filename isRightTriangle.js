e075955281939d4eb2b17584f3681b21996e7f40
Cindy Zheng <czheng10@stuy.edu>
2020-04-25 22:42:29 -0400

    trial2

--- isRightTriangle.js
+++ isRightTriangle.js
@@ -1,9 +1,10 @@
 const isRightTriangle = (a, b, c) => {
+    if (a+b<=c || b+c <=a || a+c <=b) return false;
     return ((Math.pow(a, 2) + Math.pow(b,2) === Math.pow(c,2))
 	    || (Math.pow(c, 2) + Math.pow(b,2) === Math.pow(a,2))
 	    || (Math.pow(a, 2) + Math.pow(c,2) === Math.pow(b,2))
 	   )    
 };
 
-console.log(isRightTriangle(4,3,5));
+console.log(isRightTriangle(0,0,0));
 module.exports = isRightTriangle;
